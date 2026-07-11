1fdf9186986dbf1223451dcebf7734b5fc8a3c7b
NitzanA <nitanaloni1406@gmail.com>
2026-07-11 19:22:50 +0300

    fix: Code optimization

--- mux4_1/mux4_1.v
+++ mux4_1/mux4_1.v
@@ -1,7 +1,7 @@
 module mux4_1(
-	input [1:0] s,
-	input [3:0] d,
-	output y
+	input wire [1:0] s,
+	input wire [3:0] d,
+	output wire y
 	);
-assign y = ((~s[0])&(~s[1])&d[0]) | (s[0]&(~s[1])&d[1]) | ((~s[0])&s[1]&d[2]) | (s[0]&s[1]&d[3]);
+assign y = d[s];
 endmodule
