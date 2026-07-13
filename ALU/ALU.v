cafe1ba5dc18e437554b418bab58d34f0880cfac
NitzanA <nitanaloni1406@gmail.com>
2026-07-13 09:08:06 +0300

    Using local parameters and adding extra operations

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -1,17 +1,29 @@
 module ALU(
 input [3:0] a,b,
-input [2:0] operation,
+input [3:0] operation,
 output reg [4:0] out
 );
+
+//local parameters for each operation
+localparam ADDITION = 4'd1;
+localparam SUBTRACTION = 4'd2;
+localparam MULTIPLICATION = 4'd3;
+localparam DIVISION = 4'd4;
+localparam AND = 4'd5;
+localparam OR = 4'd6;
+localparam NOT = 4'd7;
+localparam XOR = 4'd8;
+
 always @(*) begin
-	case(operation) 
-		4'd1: out = a + b;
-		4'd2: out = a - b;
-		4'd3: out = ~b;
-		4'd4: out = a & b;
-		4'd5: out = a | b;
-		4'd6: out = ~a;
-		4'd7: out = a ^ b;
+	case(operation)
+		ADDITION: out = a + b;
+		SUBTRACTION: out = a - b;
+		MULTIPLICATION: out = a * b;
+		DIVISION: out = a / b;
+		AND: out = a & b;
+		OR: out = a | b;
+		NOT: out = ~a;
+		XOR: out = a ^ b;
 		default: out = 0;
 	endcase
 end
